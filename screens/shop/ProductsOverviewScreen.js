511bd97c2fe0d7ec2ca65290096b6a7e7dee56a9
Alice Schofield <a.v.schofield@gmail.com>
2021-11-21 20:18:46 +1000

    Added ProductOverviewScreen initial functionality

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, FlatList, View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+
+const ProductsOverviewScreen = props => {
+    const products = useSelector(state => state.products.availableProducts);
+    return (
+        <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+    );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
+};
+
+const styles = StyleSheet.create({});
+
+
+
+export default ProductsOverviewScreen;
