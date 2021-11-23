318f2c047ae58d81371992fe47c461525f158b0d
Alice Schofield <a.v.schofield@gmail.com>
2021-11-23 15:06:35 +1000

    Add touchable components to view product detail data

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 import Colors from "../../constants/Colors";
 
 const ProductItem = props => {
+
+    let TouchableCmp = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
     return (
         <View style={styles.product}>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{ uri: props.image }} />
-            </View>
+            <View style={styles.touchable}>
+                <TouchableCmp onPress={props.onViewDetail} useForeground>
 
-            <View style={styles.details}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.price}>$ {props.price.toFixed(2)}</Text>
-            </View>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image} source={{ uri: props.image }} />
+                    </View>
 
-            <View style={styles.actions}>
-                <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
-                <Button color={Colors.primary} title="View Cart" onPress={props.onAddToCart} />
+                    <View style={styles.details}>
+                        <Text style={styles.title}>{props.title}</Text>
+                        <Text style={styles.price}>$ {props.price.toFixed(2)}</Text>
+                    </View>
+
+                    <View style={styles.actions}>
+                        <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
+                        <Button color={Colors.primary} title="View Cart" onPress={props.onAddToCart} />
+                    </View>
+                </TouchableCmp>
             </View>
         </View>
+
     );
 };
 
@@ -32,7 +44,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor: 'white',
         height: 300,
-        margin: 20
+        margin: 20,
+
     },
     image: {
         width: '100%',
@@ -62,7 +75,11 @@ const styles = StyleSheet.create({
         height: '60%',
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
-        overflow: 'hidden'
+        //  overflow: 'hidden'
+    },
+    touchable: {
+        overflow: 'hidden',
+        borderRadius: 10
     }
 });
 
