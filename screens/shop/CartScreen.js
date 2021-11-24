7c2f01c2fe66c44784aed12c4068049522f6f954
Alice Schofield <a.v.schofield@gmail.com>
2021-11-24 10:10:40 +1000

    Add cart delete button functionality

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import Colors from '../../constants/Colors';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -17,8 +18,10 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        // Return array with consistent sorting of elements in the cart
+        return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1);
     });
+    const dispatch = useDispatch();
 
     return (
         <View style={styles.screen}>
@@ -34,7 +37,9 @@ const CartScreen = props => {
                 renderItem={itemData => <CartItem quantity={itemData.item.quantity}
                     title={itemData.item.productTitle}
                     amount={itemData.item.sum}
-                    onRemove={() => { }} />}
+                    onRemove={() => {
+                        dispatch(cartActions.removeFromCart(itemData.item.productId));
+                    }} />}
 
 
             />
