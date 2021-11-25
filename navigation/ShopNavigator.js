fa20e8887b0e2d27b69847b5278d6f80ffec6330
Alice Schofield <a.v.schofield@gmail.com>
2021-11-25 10:36:23 +1000

    Initial setup of Admin dashboard

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -7,6 +7,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
 
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
@@ -58,9 +59,29 @@ const OrdersNavigator = createStackNavigator({
     defaultNavigationOptions: defaultNavOptions
 });
 
+
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductsScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons
+                name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                size={23}
+                color={drawerConfig.tintColor}
+
+            />
+        )
+    },
+    defaultNavigationOptions: defaultNavOptions
+});
+
+
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
